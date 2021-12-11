013066b0db9b92f339d870af5a00613a834dd1bc
violet <violet@DESKTOP-RCPPLS4>
2021-12-11 22:54:25 +0900

    router modify

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,6 +1,8 @@
-import  { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { useState } from "react";
 
 const AppRouter = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
     return (
         <Router>
             <Switch>
