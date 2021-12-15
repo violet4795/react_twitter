2811b0b4a7665cc654a02b44119b11bcd2d0fac4
violet <violet4795@naver.com>
2021-12-15 23:02:59 +0900

    firebase connect

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
 // import logo from './logo.svg';
 // import './App.css';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AppRouter from "components/Router";
 import { authService } from "fbase";
 
 function App() {
   const [ isLoggedIn, setIsLoggedIn ] = useState(false);
-  console.log(authService.currentUser);
+  
+  //https://ko.reactjs.org/docs/hooks-effect.html
+  //두번째 인자로 배열을 넘겨서, 배열안의 변수가 변하지 않는다면 effect를 막는식으로
+  //렌더링 될떄마다 effect가 발동되는것을 막을 수 있다.
+  //빈배열을 넘긴다면, effect가 일어나지 않음을 의미
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => console.log(user));
+  }, [])
+
+  
   return <>
   <AppRouter isLoggedIn={isLoggedIn} />
   <footer> &copy; {new Date().getFullYear()} React Twitter </footer>
--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,3 +1,66 @@
-const Auth = () => <span>Auth</span>;
+import { useState } from "react";
+import { authService } from "fbase";
+
+const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
+    
+    const onChange = e => {
+        // const {
+        //     target: {name, value},
+        // } = e;
+        // 왜이렇게 할당하지? 굳이 구조분해할당을 이렇게써야하나
+        if(e.target.name === 'email'){
+            setEmail(e.target.value);
+        } else if(e.target.name === 'password'){
+            setPassword(e.target.value);
+        }
+    }
+
+    const onSubmit = async e =>{
+        e.preventDefault();
+        try{
+            let data;
+            if (newAccount) {
+                //create new Account
+                data = await authService.createUserWithEmailAndPassword(email, password);
+            } else {
+                //log in
+                data = await authService.signInWithEmailAndPassword(email, password);
+            }
+            console.log(data);
+        } catch (e) {
+            setError(e.message);
+        }
+    }
+    const toggleAccount = () => setNewAccount((prev) => !prev);
+
+    const onSocialClick = e => {
+        console.log(e.target.name);
+    }
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input name="email" type="email" placeholder="Email" required 
+                value={email}
+                onChange={onChange}/>
+                <input name="password" type="password" placeholder="Password" required 
+                value={password}
+                onChange={onChange}/>
+                <input type="submit" placeholder="value" required value={newAccount? "Create Account" : "Log In"} />
+                {error}
+            </form>
+            <span onClick={toggleAccount}>
+                {newAccount ? 'Sign In' : 'Create Account'}
+            </span>
+            <div>
+                <button onClick={onSocialClick} name="google">Continue with Google</button>
+                <button onClick={onSocialClick} name="github">Continue with Github</button>
+            </div>
+        </div>
+    )
+}
 
 export default Auth;
