8469ae84fbee1e982389871dbde8556b5f1c2e95
violet <violet4795@naver.com>
2022-01-02 23:47:57 +0900

    add create tweet, read tweet

--- src/fbase.js
+++ src/fbase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
-
+import "firebase/compat/firestore";
 // import firebase from "firebase/app"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -20,3 +20,4 @@ firebase.initializeApp(firebaseConfig)
 
 export const firebaseInstance = firebase;
 export const authService = firebase.auth();
+export const dbService = firebase.firestore();
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,26 @@
-import { useState } from "react";
+import { dbService } from "fbase";
+import { useEffect, useState } from "react";
 
 const Home = () => {
-    const [nweet, setNweet] = useState("");
+    const [tweet, setTweet] = useState("");
 
-    const onSubmit = (event) => {
+    const getTweets = async () => {
+        const dbTweets = await dbService.collection("tweet").get();
+        console.log(dbTweets)
+        dbTweets.forEach(doc => console.log(doc.data()))
+    }
+
+    useEffect(() =>{
+        getTweets()
+    }, []);
+
+    const onSubmit = async (event) => {
         event.preventDefault();
+        await dbService.collection("tweet").add({
+            text: tweet,
+            createdAt: Date.now(),
+        })
+        setTweet("")
     }
 
     const onChange = (event) => {
@@ -12,19 +28,19 @@ const Home = () => {
         const {
             target: { value },
         } = event;
-        setNweet(value);
+        setTweet(value);
     };
 
     return (
         <form onSubmit={onSubmit}>
             <input
-            value={nweet}
+            value={tweet}
             onChange={onChange}
             type="text"
             placeholder="What's on your mind?"
             maxLength={120}
             />
-            <input type="submit" value="Nweet" />
+            <input type="submit" value="Tweet" />
         </form>
     )
 }
